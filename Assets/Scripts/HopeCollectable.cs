043ed279bc94178576a3851225bb402527fff69c
ThePatrickLauderdale <40611013+ThePatrickLauderdale@users.noreply.github.com>
2019-07-01 16:31:36 -0700

    Rough implementation of Hope scripts
    Attach HopeCollectable scripts to object. specify in inscpector what zone they go in.
    Attach HopeSpawnPoint script to spawned object. Tag them to a matching spawn zone.
    Ex. scene hopeItemTest.

---
+++ Assets/Scripts/HopeCollectable.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HopeCollectable : MonoBehaviour
+{
+    public GameObject[] spawnPoints;
+    public List<GameObject> openSpawnPoints;
+    public int index;
+    public HopeSpawnPoint spawnPoint;
+    public PlayerMovement playerMovement;
+    public string spawnZoneTag;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //if(spawnPoints == null)
+        spawnPoints = GameObject.FindGameObjectsWithTag(spawnZoneTag);
+        foreach(GameObject hopeSpawn in spawnPoints)
+        {
+            if (!hopeSpawn.GetComponent<HopeSpawnPoint>().occupied) openSpawnPoints.Add(hopeSpawn);
+        }
+      
+        if(spawnPoints.Length == 0)
+        {
+            Debug.Log("no more available spawn points with tag: " + spawnZoneTag);
+        }
+        else
+        {
+            index = Random.Range(0, openSpawnPoints.Count);
+            spawnPoint = openSpawnPoints[index].GetComponent<HopeSpawnPoint>();
+            transform.position = openSpawnPoints[index].transform.position;
+            spawnPoint.occupied = true;
+
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            playerMovement = collision.gameObject.GetComponent<PlayerMovement>();
+            playerMovement.jumpInc += 0.1f;
+            Destroy(gameObject);
+        }
+
+    }
+}
