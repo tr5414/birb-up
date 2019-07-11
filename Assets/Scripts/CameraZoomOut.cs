3246c216b71e3fd723ff252bafd7b807c8c7cce7
Thelonius Erickson <theoserickson@gmail.com>
2019-07-11 16:09:58 -0700

    Camera Zoom Out Works for All Directions & Added .vs and obj folders to .gitignore

--- Assets/Scripts/CameraZoomOut.cs
+++ Assets/Scripts/CameraZoomOut.cs
@@ -11,7 +11,8 @@ public class CameraZoomOut : MonoBehaviour
     public GameObject player;
 
     public float defaultSize;
-    public float horizontalCameraBuffer = 5; //Distance from horizontal edge of camera you are before it zooms out. This allows you to move within the camera bounds without the zoom changing 
+    public float horizontalCameraBuffer = 5; //Distance from horizontal edge of camera you have to be before it zooms out. This allows you to move within the camera bounds without the zoom changing 
+    public float verticalCameraBuffer = 5; 
 
     // Start is called before the first frame update
     void Start()
@@ -25,37 +26,18 @@ public class CameraZoomOut : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //X
-        print("player pos.x:"+ player.transform.position.x + " cam pos.x + ortho size:" + cam.transform.position.x + cameraScript.orthographicSize);
-        print("cam ortho size:" + cameraScript.orthographicSize + " default size (The smallest it should go) :" + defaultSize);
-        if (Mathf.Abs(player.transform.position.x) + horizontalCameraBuffer > cam.transform.position.x + cameraScript.orthographicSize)
+        if (Mathf.Abs(player.transform.position.x) + horizontalCameraBuffer > cam.transform.position.x + cameraScript.orthographicSize || Mathf.Abs(player.transform.position.y) + verticalCameraBuffer > cam.transform.position.y + cameraScript.orthographicSize)
         {
             print("expanding camera x");
             cameraScript.orthographicSize += 0.1f;
 
-            //if you are below camera bounds, minimize camera view. DO NOT do so though, if the camera is at it's defaultSize (So you don't get super zoomed in)
-        }else if (Mathf.Abs(player.transform.position.x) - horizontalCameraBuffer < cam.transform.position.x + cameraScript.orthographicSize
-          && cameraScript.orthographicSize > defaultSize)
+        //if you are below camera bounds, minimize camera view. DO NOT do so though, if the camera is at it's defaultSize (So you don't get super zoomed in)
+        }
+        else if (cameraScript.orthographicSize > defaultSize)
         {
             print("contracting camera x");
             cameraScript.orthographicSize -= 0.1f;
-        }
-        
-        /*
+        }        
 
-    //Y
-        //if the player.y is above the camera viewport height, expand out the size
-        if (player.transform.position.y > cam.transform.position.y + cameraScript.orthographicSize)
-        {
-            cameraScript.orthographicSize+= 0.1f;
-
-        //if you are below camera bounds, minimize camera view. DO NOT do so though, if the camera is at it's defaultSize (So you don't get super zoomed in)
-        } else if(player.transform.position.y < cam.transform.position.y + cameraScript.orthographicSize 
-            && cameraScript.orthographicSize > defaultSize)
-        {
-            cameraScript.orthographicSize-= 0.1f;
-        }
-
-    */
     }
 }
