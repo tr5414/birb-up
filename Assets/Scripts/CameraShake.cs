417fd5d92194ecca2f99748c241143c217c387a3
Thelonius Erickson <theoserickson@gmail.com>
2019-07-11 14:44:31 -0700

    Added Camera Shake and 1st Pass On Camera Zoom Out

---
+++ Assets/Scripts/CameraShake.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    public bool debug = false;
+
+    public float shakeAmount = 0.5f;
+    public float shakeLength = 1.0f;
+
+    public bool smoothing;
+    public float smoothAmount = 5f;
+
+    bool isRunning = false;
+
+
+    float shakePercent;
+    float startAmount;
+    float startDuration;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (debug) Shake();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void ShakeCamera()
+    {
+        startAmount = shakeAmount;
+        startDuration = shakeLength;
+
+        if (!isRunning) StartCoroutine(Shake());
+    }
+
+
+    public void ShakeCamera(float amount, float duration)
+    {
+        shakeAmount += amount;
+        shakeLength += duration;
+
+        ShakeCamera();
+    }
+
+    IEnumerator Shake()
+    {
+        isRunning = true;
+
+        while (shakeLength > 0.01f)
+        {
+            Vector3 rotationAmt = Random.insideUnitSphere * shakeLength;
+            rotationAmt.z = 0;
+
+            shakePercent = shakeLength / startDuration;
+
+            shakeAmount = startAmount * shakePercent;
+            shakeLength = Mathf.Lerp(shakeLength, 0, Time.deltaTime);
+
+
+            if (smoothing)
+            {
+                transform.localRotation = Quaternion.Lerp(transform.localRotation, Quaternion.Euler(rotationAmt), Time.deltaTime * smoothAmount);
+            }
+            else
+            {
+                transform.localRotation = Quaternion.Euler(rotationAmt);
+            }
+
+            yield return null;
+        }
+        transform.localRotation = Quaternion.identity;
+        isRunning = false;
+    }
+
+}
---
+++ Assets/Scripts/CameraZoomOut.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoomOut : MonoBehaviour
+{
+
+    public GameObject cam;
+    public Camera cameraScript;
+
+    public GameObject player;
+
+    public float defaultSize;
+    public float horizontalCameraBuffer = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!cam) cam = Camera.main.transform.gameObject;
+        if (!cameraScript) cameraScript = cam.GetComponent<Camera>();
+        if (!player) player = GameObject.Find("Player");
+        defaultSize = cameraScript.orthographicSize;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //X
+        print("play:"+ player.transform.position.x + " cam:" + cam.transform.position.x + cameraScript.orthographicSize);
+        print("cam:" + cameraScript.orthographicSize + " def:" + defaultSize);
+        if (Mathf.Abs(player.transform.position.x) + horizontalCameraBuffer > cam.transform.position.x + cameraScript.orthographicSize)
+        {
+            print("expand x");
+            cameraScript.orthographicSize += 0.1f;
+
+            //if you are below camera bounds, minimize camera view. DO NOT do so though, if the camera is at it's defaultSize (So you don't get super zoomed in)
+        }else if (Mathf.Abs(player.transform.position.x) - horizontalCameraBuffer < cam.transform.position.x + cameraScript.orthographicSize
+          && cameraScript.orthographicSize > defaultSize)
+        {
+            print("contract x");
+            cameraScript.orthographicSize -= 0.1f;
+        }
+        
+        /*
+
+    //Y
+        //if the player.y is above the camera viewport height, expand out the size
+        if (player.transform.position.y > cam.transform.position.y + cameraScript.orthographicSize)
+        {
+            cameraScript.orthographicSize+= 0.1f;
+
+        //if you are below camera bounds, minimize camera view. DO NOT do so though, if the camera is at it's defaultSize (So you don't get super zoomed in)
+        } else if(player.transform.position.y < cam.transform.position.y + cameraScript.orthographicSize 
+            && cameraScript.orthographicSize > defaultSize)
+        {
+            cameraScript.orthographicSize-= 0.1f;
+        }
+
+    */
+    }
+}
